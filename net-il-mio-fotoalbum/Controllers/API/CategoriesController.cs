502a41bf71bb892e2bdc3e2af8f435090c2841c4
broyettore <broyettore@gmail.com>
2023-10-13 16:52:19 +0200

    refactoring and styling to match front end done

--- net-il-mio-fotoalbum/Controllers/API/CategoriesController.cs
+++ net-il-mio-fotoalbum/Controllers/API/CategoriesController.cs
@@ -1,27 +1,124 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using net_il_mio_fotoalbum.Database;
 using net_il_mio_fotoalbum.Models;
 
 namespace net_il_mio_fotoalbum.Controllers.API
 {
-    [Route("api/[controller]/[action]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class CategoriesController : ControllerBase
     {
-        private readonly PhotoContext _myDb;
+        private readonly PhotoContext _context;
 
-        public CategoriesController(PhotoContext myDb)
+        public CategoriesController(PhotoContext context)
         {
-            _myDb = myDb;
+            _context = context;
         }
 
+        // GET: api/Categories
         [HttpGet]
-        public IActionResult GetCategories()
+        public async Task<ActionResult<IEnumerable<Category>>> GetCategories()
         {
-            List<Category> categories = _myDb.Categories.ToList();
+          if (_context.Categories == null)
+          {
+              return NotFound();
+          }
+            return await _context.Categories.ToListAsync();
+        }
 
-            return Ok(categories);
+        // GET: api/Categories/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetCategory(int id)
+        {
+          if (_context.Categories == null)
+          {
+              return NotFound();
+          }
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return category;
+        }
+
+        // PUT: api/Categories/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategory(int id, Category category)
+        {
+            if (id != category.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Categories
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Category>> PostCategory(Category category)
+        {
+          if (_context.Categories == null)
+          {
+              return Problem("Entity set 'PhotoContext.Categories'  is null.");
+          }
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCategory", new { id = category.Id }, category);
+        }
+
+        // DELETE: api/Categories/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            if (_context.Categories == null)
+            {
+                return NotFound();
+            }
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return (_context.Categories?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
